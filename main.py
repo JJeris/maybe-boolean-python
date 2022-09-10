61bdfbb9c8e07b9739e78f1ffedbd0e880cf7b30
Johans Justs Eris <johans.eris@r6vsk.lv>
2022-09-10 20:34:58 +0300

    Finished the main mechanism.

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ from maybe_boolean import MaybeBoolean as Maybe
 
 maybe = Maybe()
 
-print("I cant decide what to do.")
-
-answer = int(input("Your boolean:"))
-
-if maybe.maybe():
+if maybe.Maybe:
     print("True")
 else:
     print("False")
--- maybe_boolean.py
+++ maybe_boolean.py
@@ -4,11 +4,4 @@ class MaybeBoolean:
     """Class for a 'Maybe' boolean."""
     
     def __init__(self):
-        self.Maybe = bool(self.maybe())
-        
-    def maybe(self):
-        Maybe = random.randint(0,1)
-        return Maybe
-
-# Maybe = MaybeBoolean()
-# print(bool(Maybe._return_value()))
+        self.Maybe = bool(random.randint(0,1))
